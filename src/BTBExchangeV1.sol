e5fb95f918cfbb0b85952d5c80ad3ee1fbacb029
i own ducks <111690356+iownducks@users.noreply.github.com>
2025-02-25 09:56:44 +0100

    Update BTBExchangeV1.sol

--- src/BTBExchangeV1.sol
+++ src/BTBExchangeV1.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -17,39 +16,28 @@ contract BTBExchangeV1 is Ownable, ReentrancyGuard, Pausable {
     address public adminAddress;
     uint256 public constant PRECISION = 1e6;
     uint256 public constant TOKEN_PRECISION = 1e18;
-    uint256 public totalTokensSold;
     
     uint256 public usdcBorrowed;
     uint256 public buyFee = 30;
     uint256 public sellFee = 30;
-    uint256 public constant ADMIN_FEE_PORTION = 10;
-    uint256 public constant PRICE_CONTRIBUTION_PORTION = 20;
+    uint256 public adminFee = 10;
     uint256 public constant FEE_PRECISION = 10000;
     uint256 public lastTradeBlock;
     uint256 public constant MIN_PRICE = 10000;
     uint256 public constant MIN_EFFECTIVE_CIRCULATION = 1e16;
     uint256 public constant MAX_INITIAL_PRICE = 100000;
-    bool public emergencyMode;
 
-    event FeesUpdated(uint256 newBuyFee, uint256 newSellFee);
+    event FeesUpdated(uint256 newBuyFee, uint256 newSellFee, uint256 newAdminFee);
     event AdminAddressUpdated(address indexed newAdmin);
     event TokensWithdrawn(address indexed token, uint256 amount);
-    event EmergencyModeSet(bool activated);
     event UsdcBorrowed(uint256 amount, uint256 totalBorrowed);
     event UsdcRepaid(uint256 amount, uint256 remainingBorrowed);
 
     error SameBlockTrade();
     error PriceBelowMinimum();
-    error ZeroCirculatingSupply();
     error InsufficientReserve();
-    error EmergencyModeActive();
     error InvalidAmount();
     error TransferFailed();
-
-    modifier notEmergency() {
-        if (emergencyMode) revert EmergencyModeActive();
-        _;
-    }
 
     constructor(
         address _token,
@@ -104,34 +92,28 @@ contract BTBExchangeV1 is Ownable, ReentrancyGuard, Pausable {
         return usdc.balanceOf(address(this));
     }
 
-    function buyTokens(uint256 usdcAmount) external nonReentrant whenNotPaused notEmergency {
+    function buyTokens(uint256 usdcAmount) external nonReentrant whenNotPaused {
         if (usdcAmount == 0) revert InvalidAmount();
         if (block.number == lastTradeBlock) revert SameBlockTrade();
 
         uint256 totalFeeAmount = (usdcAmount * buyFee) / FEE_PRECISION;
-        uint256 adminFeeAmount = (usdcAmount * ADMIN_FEE_PORTION) / FEE_PRECISION;
-        uint256 priceContributionAmount = (usdcAmount * PRICE_CONTRIBUTION_PORTION) / FEE_PRECISION;
+        uint256 adminFeeAmount = (usdcAmount * adminFee) / FEE_PRECISION;
         uint256 usdcAfterFee = usdcAmount - totalFeeAmount;
         
         uint256 price = getCurrentPrice();
         uint256 tokenAmount = (usdcAfterFee * TOKEN_PRECISION) / price;
         
         require(token.balanceOf(address(this)) >= tokenAmount, "Insufficient tokens in contract");
-   
-        bool success1 = usdc.transferFrom(msg.sender, address(this), usdcAfterFee + priceContributionAmount);
-        if (!success1) revert TransferFailed();
-        
-        bool success2 = usdc.transferFrom(msg.sender, adminAddress, adminFeeAmount);
-        if (!success2) revert TransferFailed();
 
-        totalTokensSold += tokenAmount;
+        usdc.safeTransferFrom(msg.sender, address(this), usdcAmount - adminFeeAmount);
+        usdc.safeTransferFrom(msg.sender, adminAddress, adminFeeAmount);
+        
         lastTradeBlock = block.number;
 
-        bool success3 = token.transfer(msg.sender, tokenAmount);
-        if (!success3) revert TransferFailed();
+        token.safeTransfer(msg.sender, tokenAmount);
     }
 
-    function sellTokens(uint256 tokenAmount) external nonReentrant whenNotPaused notEmergency {
+    function sellTokens(uint256 tokenAmount) external nonReentrant whenNotPaused {
         if (tokenAmount == 0) revert InvalidAmount();
         if (block.number == lastTradeBlock) revert SameBlockTrade();
 
@@ -139,37 +121,29 @@ contract BTBExchangeV1 is Ownable, ReentrancyGuard, Pausable {
         
         uint256 usdcAmount = (tokenAmount * price) / TOKEN_PRECISION;
         uint256 totalFeeAmount = (usdcAmount * sellFee) / FEE_PRECISION;
-        uint256 adminFeeAmount = (usdcAmount * ADMIN_FEE_PORTION) / FEE_PRECISION;
+        uint256 adminFeeAmount = (usdcAmount * adminFee) / FEE_PRECISION;
         uint256 usdcAfterFee = usdcAmount - totalFeeAmount;
 
-        uint256 totalUsdcNeeded = usdcAfterFee;
-        if (adminFeeAmount > 0) {
-            totalUsdcNeeded += adminFeeAmount;
-        }
+        uint256 totalUsdcNeeded = usdcAfterFee + adminFeeAmount;
         
         if (totalUsdcNeeded > usdc.balanceOf(address(this))) revert InsufficientReserve();
 
-        bool success1 = token.transferFrom(msg.sender, address(this), tokenAmount);
-        if (!success1) revert TransferFailed();
+        token.safeTransferFrom(msg.sender, address(this), tokenAmount);
         
-        totalTokensSold -= tokenAmount;
         lastTradeBlock = block.number;
         
-        bool success2 = usdc.transfer(msg.sender, usdcAfterFee);
-        if (!success2) revert TransferFailed();
-        
-        if (adminFeeAmount > 0) {
-            bool success3 = usdc.transfer(adminAddress, adminFeeAmount);
-            if (!success3) revert TransferFailed();
-        }
+        usdc.safeTransfer(msg.sender, usdcAfterFee);
+        usdc.safeTransfer(adminAddress, adminFeeAmount);
     }
 
-    function updateFees(uint256 newBuyFee, uint256 newSellFee) external onlyOwner {
+    function updateFees(uint256 newBuyFee, uint256 newSellFee, uint256 newAdminFee) external onlyOwner {
         require(newBuyFee <= FEE_PRECISION, "Buy fee too high");
         require(newSellFee <= FEE_PRECISION, "Sell fee too high");
+        require(newAdminFee <= FEE_PRECISION, "Admin fee too high");
         buyFee = newBuyFee;
         sellFee = newSellFee;
-        emit FeesUpdated(newBuyFee, newSellFee);
+        adminFee = newAdminFee;
+        emit FeesUpdated(newBuyFee, newSellFee, newAdminFee);
     }
 
     function updateAdminAddress(address newAdmin) external onlyOwner {
@@ -179,6 +153,7 @@ contract BTBExchangeV1 is Ownable, ReentrancyGuard, Pausable {
     }
 
     function borrowUsdc(uint256 amount) external onlyOwner {
+        require(amount > 0, "Amount must be greater than zero");
         uint256 availableToWithdraw = usdc.balanceOf(address(this));
         require(amount <= availableToWithdraw, "Cannot borrow from reserves");
         
@@ -199,26 +174,8 @@ contract BTBExchangeV1 is Ownable, ReentrancyGuard, Pausable {
     }
     
     function recoverERC20(address tokenAddress, uint256 amount) external onlyOwner {
-        if (tokenAddress == address(usdc)) {
-            uint256 minLiquidity = totalTokensSold * MIN_PRICE / TOKEN_PRECISION;
-            require(usdc.balanceOf(address(this)) - amount >= minLiquidity, "Must maintain minimum liquidity");
-        } else if (tokenAddress == address(token)) {
-            uint256 excessTokens = token.balanceOf(address(this)) - totalTokensSold;
-            require(amount <= excessTokens, "Cannot withdraw from circulating supply");
-        }
         IERC20(tokenAddress).safeTransfer(owner(), amount);
-        
         emit TokensWithdrawn(tokenAddress, amount);
-    }
-
-    function setEmergencyMode(bool activated) external onlyOwner {
-        emergencyMode = activated;
-        if (activated) {
-            _pause();
-        } else {
-            _unpause();
-        }
-        emit EmergencyModeSet(activated);
     }
 
     function pause() external onlyOwner {
@@ -226,7 +183,6 @@ contract BTBExchangeV1 is Ownable, ReentrancyGuard, Pausable {
     }
 
     function unpause() external onlyOwner {
-        require(!emergencyMode, "Cannot unpause in emergency mode");
         _unpause();
     }
     
